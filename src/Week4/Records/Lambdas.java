720c73eab2e7b7c2cd417d73020e48e83ec62012
Yaren Erol <yarenerol1@icloud.com>
2025-08-17 09:25:39 +0300

    Lambdas.java dosyası eklendi.

---
+++ src/Week4/Records/Lambdas.java
@@ -0,0 +1,32 @@
+package Week4.Records;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lambdas {
+    public static void main(String[] args) {
+
+        // Öncelikle sayıları tuttuğumuz bir arrayList oluşturalım.
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+
+        // Şimdi stream ile bu listenin tüm öğelerini gezelim.
+        // Ardından lambda ifadesiyle iki katına çıkaralım.
+        // Son olarak da bulduğumuz değerleri bir listede toplayalım.
+
+        List<Integer> doubledNumbers = numbers.stream().map(num -> (num * 2)).collect(Collectors.toList());
+
+        // Ve yazdıralım
+
+        System.out.println(doubledNumbers);
+
+
+    }
+}
